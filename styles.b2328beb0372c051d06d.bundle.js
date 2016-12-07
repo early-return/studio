15f2171270bb34c901bba5df9fff63f4e0a99a61
Zhiqing <i@zhiqing.info>
2016-12-07 20:32:59 +0800

    completed work detail page

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{378:function(n,o){},688:function(n,o,c){n.exports=c(378)}},[688]);
+webpackJsonp([1,2],{378:function(n,o){},685:function(n,o,c){n.exports=c(378)}},[685]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
