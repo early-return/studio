3329f7d340d4ba0284ac93fb7b8864ceabc2cd4a
Zhiqing <i@zhiqing.info>
2016-12-11 17:10:41 +0800

    added 404 page to router

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{378:function(n,o){},685:function(n,o,c){n.exports=c(378)}},[685]);
+webpackJsonp([1,2],{379:function(n,o){},683:function(n,o,c){n.exports=c(379)}},[683]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
