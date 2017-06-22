8073e8e6509955624faf3b9633a5755b003cc795
Zhiqing <i@zhiqing.info>
2017-06-22 16:07:28 +0800

    delete all

--- src/app/shared/components/icon.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component, Input} from "@angular/core";
-@Component({
-  selector: 'icon',
-  template: `<span [class]="iconClass"></span>`
-})
-export class IconComponent {
-  @Input() iconClass: string;
-}
